2c596f33f44d19626e4fed227a666cdcde23387e
rodrigoacs <rodrigohths@gmail.com>
2024-04-04 21:25:23 -0300

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="q-pa-md">
-    <div class="text-h5">Bem vindo ao RVendas.</div>
-    <div class="text-h6">{{ currentDate }}</div>
+    <div class="text-h5">Bem vindo ao RVendas. {{ currentDate }}</div>
   </div>
 
 </template>
